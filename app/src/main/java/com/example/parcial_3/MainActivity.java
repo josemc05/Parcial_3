2b461df4b6d4897100254054e999b2ffbc3959f9
josemc05 <cuevasjose05@gmail.com>
2020-06-24 12:12:21 -0500

    Prueba de Actualizacion

--- app/src/main/java/com/example/parcial_3/MainActivity.java
+++ app/src/main/java/com/example/parcial_3/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.parcial_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //Cuevitas estuvo aqui
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Parcial3BDhelper base = new Parcial3BDhelper(getApplicationContext());
+        base.agregarvaloresiniciales();
+
     }
 }
---
+++ app/src/main/java/com/example/parcial_3/Parcial3BDhelper.java
@@ -0,0 +1,52 @@
+package com.example.parcial_3;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+class Parcial3BDhelper extends SQLiteOpenHelper {
+
+    String tableUsuario = "CREATE TABLE usuarios(id_u INTEGER primary key autoincrement, nombre TEXT, password TEXT, tipo_u TEXT)";
+    String tableCategoria = "CREATE TABLE recetas(id_r INTEGER primary key autoincrement, producto TEXT, ingrediente1 TEXT, ingrediente2 TEXT, ingrediente3 TEXT," +
+            "ingrediente4 TEXT,ingrediente5 TEXT,preparacion TEXT)";
+    String tableGuardado = "CREATE TABLE recetas_guardadas(id_u_fkg INTEGER,id_r_fkg INTEGER, gusto TEXT, original TEXT, primary key (id_u_fkg, id_r_fkg)" +
+            ", foreign key(id_u_fkg) references usuarios(id_u), foreign key(id_r_fkg) references recetas(id_r))";
+    String tableFavorito = "CREATE TABLE recetas_fav(id_u_fkf INTEGER,id_r_fkf INTEGER, primary key (id_u_fkg, id_r_fkg)" +
+            ", foreign key(id_u_fkf) references usuarios(id_u), foreign key(id_r_fkg) references recetas(id_r))";
+    //String nombreBD = "Recetario";
+    //int versionBD= 1;
+
+    public Parcial3BDhelper (Context context){
+        super(context,"Recetario",null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(tableUsuario);
+        db.execSQL(tableCategoria);
+        db.execSQL(tableGuardado);
+        db.execSQL(tableFavorito);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+public void agregarvaloresiniciales(){
+    SQLiteDatabase db = getWritableDatabase();
+
+    if (db != null){
+        ContentValues values = new ContentValues();
+        values.put("nombre","jose");
+        values.put("password","123");
+        values.put("tipo_u","admin");
+
+
+        db.insert("usuarios",null,values);
+    }
+}
+
+}
