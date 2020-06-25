6d9cce4113a6b8364071029e22a6cc6749a02c74
Greibaam <melo.near.m@gmail.com>
2020-06-25 01:18:12 -0500

    Se agrego la interfaz de lista de receta

---
+++ app/src/main/java/com/example/parcial_3/Descripcion_Receta.java
@@ -0,0 +1,14 @@
+package com.example.parcial_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Descripcion_Receta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_descripcion__receta);
+    }
+}
