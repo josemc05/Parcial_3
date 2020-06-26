90d6a183e85113aeb52d5f41a35cd0bf1f5f7039
josemc05 <cuevasjose05@gmail.com>
2020-06-25 19:46:15 -0500

    Agregada la pantalla de "Ver recetas Guardadas" lista falta probarla

--- app/src/main/java/com/example/parcial_3/Adapterlistview.java
+++ app/src/main/java/com/example/parcial_3/Adapterlistview.java
@@ -25,13 +25,16 @@ class Adapterlistview extends ArrayAdapter<recetas> {
         context1=context;
     }
 
+
+
     public View getView(int position, View convertView, ViewGroup parent){
         LayoutInflater inflater = LayoutInflater.from(getContext());
         View item = inflater.inflate(R.layout.listviewrecetas, null);
-
+        //colocar los productos
         TextView producto = (TextView)item.findViewById(R.id.nombrereceta);
         producto.setText(receta.get(position).getProduc());
 
+        //Colocar la imagen basado en la ruta de la imagen
         ImageView imagenp = (ImageView) item.findViewById(R.id.imgrecetalista);
         String img=receta.get(position).getImagen();
         String uri ="drawable/"+img;
@@ -39,10 +42,37 @@ class Adapterlistview extends ArrayAdapter<recetas> {
         Drawable foto = ContextCompat.getDrawable(context1, id);
         imagenp.setImageDrawable(foto);
 
+        //Establecer el imageView/ImageSwitcher/ImageButton
+        ImageView imageng = (ImageView) item.findViewById(R.id.botonimagen1);
+        String imagen1 = receta.get(position).getGusto();
+        if(imagen1.equals("si")){
+            String uri1 ="drawable/gustar";
+            int id1 = context1.getResources().getIdentifier(uri1 , null, context1.getPackageName());
+            Drawable foto1 = ContextCompat.getDrawable(context1, id1);
+            imageng.setImageDrawable(foto1);
+        }
+        else{
+            String uri2 ="drawable/nogustar";
+            int id2 = context1.getResources().getIdentifier(uri2 , null, context1.getPackageName());
+            Drawable foto2 = ContextCompat.getDrawable(context1, id2);
+            imageng.setImageDrawable(foto2);
+        }
 
-
-
-
+        //Establecer el imageView/ImageSwitcher/ImageButton 2
+        ImageView imageno = (ImageView) item.findViewById(R.id.botonimagen2);
+        String imagen2 = receta.get(position).getOriginal();
+        if(imagen2.equals("si")){
+            String uri3 ="drawable/original";
+            int id3 = context1.getResources().getIdentifier(uri3 , null, context1.getPackageName());
+            Drawable foto1 = ContextCompat.getDrawable(context1, id3);
+            imageno.setImageDrawable(foto1);
+        }
+        else{
+            String uri4 ="drawable/noriginal";
+            int id4 = context1.getResources().getIdentifier(uri4 , null, context1.getPackageName());
+            Drawable foto2 = ContextCompat.getDrawable(context1, id4);
+            imageno.setImageDrawable(foto2);
+        }
         return(item);
     }
 }
